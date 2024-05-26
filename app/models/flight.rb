6e2b3f70d9b9c3e02bbf0fc05785b5cb2d04ab67
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-26 13:49:33 -0300

    feat: Add models for passengers and bookings and new associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :arrival, class_name: "Airport"
   belongs_to :departure, class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
 end
