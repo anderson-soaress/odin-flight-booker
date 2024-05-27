d4f25bef16faa58addc1f4b5cf6990a677c78f45
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-27 11:24:31 -0300

    feat: Add create booking and change some models relations Booking accepts nested attributes for passenger, so when the user select a flight he will be redirect to booking :new to add the passengers data

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight])
+    @booking = Booking.new
+    params[:passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_143254) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_130339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,12 +21,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_143254) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "passenger_id", null: false
     t.bigint "flight_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -45,10 +43,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_143254) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
-  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "arrival_id"
   add_foreign_key "flights", "airports", column: "departure_id"
+  add_foreign_key "passengers", "bookings"
 end
