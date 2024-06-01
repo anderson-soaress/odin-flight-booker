b03a8f1114af47c3015492c3b658c4a35f860221
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-31 21:07:26 -0300

    feat: Add turbo frame to flights search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,12 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map{|airport| [airport.code, airport.id]}
+    @booking = Booking.new
 
     if params[:departure].present? && params[:arrival].present?
       @flights = Flight.where(departure_id: params[:departure], arrival_id: params[:arrival]).for_date(params[:date])
     else
-      @flights = Flight.all
+      @flights = nil
     end
   end
 end
